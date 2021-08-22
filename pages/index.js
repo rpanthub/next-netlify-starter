1b87f86694a620cb7d3a6d9855295a1b61e85fa7
rpanthub <ranjan.pant@gmail.com>
2021-08-22 15:02:41 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
           Believe in your self!!! <br />
           Remember that even a blind squirrel can find nuts once in a while!!! <br />
           We rock!!! <br />
-          I COME IN PEACE! <br />
+          When the student is ready, the teacher will appear! <br />
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
