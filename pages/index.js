d05fe638ffaa0ce13ce9abd8476bea2cd8f2bccd
RP <ranjan.pant@gmail.com>
2023-01-05 22:38:06 -0500

    Update quote

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,7 @@ export default function Home() {
           Your time is limited, so don't waste it living someone else's life.  <br />
           If life were predictable it would cease to be life, and be without flavor.  <br />
           Money Can't buy Happiness. <br />
+          My Pride and joy. <br />
         </p>
         <p className="description">
           Get started by editing <code>pages/index.js</code>
